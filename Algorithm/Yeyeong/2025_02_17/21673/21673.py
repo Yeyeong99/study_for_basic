01741680a4efdf276dac0c8934dcccc5bec6645f
Yeyeong <67695343+Yeyeong99@users.noreply.github.com>
2025-02-23 22:20:39 +0900

    Update 21673.py #1

--- Algorithm/Yeyeong/2025_02_17/21673/21673.py
+++ Algorithm/Yeyeong/2025_02_17/21673/21673.py
@@ -1,8 +1,27 @@
-T = int(input())
-
-for t in (1, T + 1):
-    numbers = list(input())
-
-    for i in range(len(numbers) - 1):
-        if numbers[i] == '*' or numbers[i] == '/':
-            numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+operators = {
+    '*': 1,
+    '/': 1,
+    '+': 2,
+    '-': 2
+}
+t = int(input())
+ 
+for tc in range(1, t + 1):
+    equation = input()
+    result = []
+    stack = []
+    for e in equation:
+        if e in operators.keys():
+            if not stack or operators[e] < operators[stack[-1]]:
+                stack.append(e)
+            else:
+                result.append(stack.pop())
+                stack.append(e)
+                 
+        else:
+            result.append(e)
+     
+    while stack:
+        result.append(stack.pop())
+ 
+    print(f"#{tc} {''.join(result)}")
