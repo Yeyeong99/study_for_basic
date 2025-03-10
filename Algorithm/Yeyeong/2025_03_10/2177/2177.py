0da6837982a29078375e40b187ed2ab96daee04b
Yeyeong99 <gongsam99@gmail.com>
2025-03-10 16:36:29 +0900

    Try with BFS

--- Algorithm/Yeyeong/2025_03_10/2177/2177.py
+++ Algorithm/Yeyeong/2025_03_10/2177/2177.py
@@ -3,49 +3,52 @@ sys.stdin = open("input.txt", 'r')
 
 from collections import deque
 
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 0]
 
-
-def area(i, j, n):
+def bfs(i, j, a):
     queue = deque()
     queue.append((i, j))
-    visited = [[False] * N for _ in range(N)]
-
+    side = [] # 가장자리 좌표 저장
+    result = [(i, j)]
     while queue:
         x, y = queue.popleft()
-        if not visited[x][y]:
-            visited[x][y] = True
-            for d in range(4):
-                for k in range(1, n):
-                    nx = x + dx[d] * k
-                    ny = y + dy[d] * k
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
 
-                    if (nx < 0 or nx >= N) or (ny < 0 or ny >= N):
-                        continue
-                    if (nx, ny) in queue:
-                        continue
-                    if visited[nx][ny]:
-                        continue
-                    queue.append((nx, ny))
-
-    result = []
-    for p in range(n):
-        for q in range(n):
-            if visited[p][q]:
-                result.append([p, q])
-
+            if (nx == i or ny == j) and len(side) < 4:
+                queue.append((nx, ny))
+                side.append((nx, ny))
+            if
+            result.append((nx, ny))
     return result
+
+
+def get_profit(i, j):
+    global max_profit
+    for a in range(N):
+        current_dia = bfs(i, j, a)
+        cost = len(current_dia)
+        profit = sum([1 for q, p in current_dia if city[q][p] == 1]) * M
+
+        if cost > profit:
+            continue
+        else:
+            max_profit = max(max_profit, profit - cost)
 
 
 T = int(input())
 
 for t in range(1, T + 1):
-    N, M = map(int, input().split()) # 도시 크기, 지불 가능 비용
+    N, M = map(int, input().split())  # 도시 크기, 지불 가능 비용
     city = [list(map(int, input().split())) for _ in range(N)]
-
+    total_houses = sum([1 for i in range(N) for j in range(N) if city[i][j] == 1])
+    max_profit = -float('inf')
     for i in range(N):
         for j in range(N):
-            for a in range(1, N):
-                current = area(i, j, a)
+            if city[i][j] == 1:
+                get_profit(i, j)
+
+    print(max_profit)
