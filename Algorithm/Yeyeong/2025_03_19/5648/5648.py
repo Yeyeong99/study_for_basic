624cb2a6f1241197f4f9d19066a21c35e6b07fd5
Yeyeong <gongsam99@gmail.com>
2025-03-19 15:29:21 +0900

    Solving Graph Algorithms

--- Algorithm/Yeyeong/2025_03_19/5648/5648.py
+++ Algorithm/Yeyeong/2025_03_19/5648/5648.py
@@ -1,29 +1,55 @@
 import sys
 sys.stdin = open("input.txt", 'r')
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
+dx = [0, 0, -1, 1]  # 상(0), 하(1), 좌(2), 우(3)
+dy = [1, -1, 0, 0]
 
 T = int(input())
-
 for t in range(1, T + 1):
     N = int(input())
     data = [list(map(int, input().split())) for _ in range(N)]
-    i = 0
+    result = 0
 
-    same_x = [] # x가 동일선상에 있는 좌표
-    same_y = []  # y가 동일선상에 있는 좌표
+    # 좌표 확장 (0.5초 단위 처리)
     for i in range(N):
-        for j in range(i + 1, N):
-            print(data[i], data[j])
-            if data[i][0] == data[j][0]:
-                if (data[i][1] > data[j][1] and data[i][2] == 2 and data[j][2] == 3) or (data[i][1] < data[j][1] and data[j][2] == 2 and data[i][2] == 3):  # 이동방향이 좌 or 우
-                    same_x.append(data[i])
-                    same_x.append(data[j])
-            if data[i][1] == data[j][1]:
-                if (data[i][0] < data[j][1] and data[i][2] == 0 and data[j][2] == 1) or (data[i][0] > data[j][1] and data[j][2] == 0 and data[i][2] == 1):  # 이동방향이 좌 or 우
-                    same_x.append(data[i])
-                    same_y.append(data[j])
+        data[i][0] = (data[i][0] + 1000) * 2  # x좌표
+        data[i][1] = (data[i][1] + 1000) * 2  # y좌표
 
-    print(same_x, same_y)
+    for _ in range(8000):  # 최대 4000초 시뮬레이션 (0.5초 단위)
+        pos_dict = {}
+        active = False
+
+        # 1. 원자 이동
+        for i in range(N):
+            x, y, d, e = data[i]
+            if e == 0:  # 소멸된 원자 건너뛰기
+                continue
+            active = True
+
+            # 새로운 좌표 계산
+            nx = x + dx[d]
+            ny = y + dy[d]
+            data[i][0] = nx
+            data[i][1] = ny
+
+            # 충돌 위치 기록
+            key = (nx, ny)
+            if key not in pos_dict:
+                pos_dict[key] = []
+            pos_dict[key].append(i)
+
+        if not active:  # 모든 원자 소멸
+            break
+
+        # 2. 충돌 검사
+        for key in pos_dict:
+            atoms = pos_dict[key]
+            if len(atoms) >= 2:
+                sum_e = 0
+                for idx in atoms:
+                    if data[idx][3] > 0:
+                        sum_e += data[idx][3]
+                        data[idx][3] = 0  # 에너지 방출
+                result += sum_e
+
+    print(f"#{t} {result}")
